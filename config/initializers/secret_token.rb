5ba78b9b159c0d4d259ce970f150399721163fca
Eric Haase <erichaase@gmail.com>
2012-02-02 10:31:58 -0600

    initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantasynba::Application.config.secret_token = 'bc2077d2f1223d1d796adc285f1767877da17cc2e4a6b35ad504c3b72250e8324e9a169c3cacec5491d418e0e0f94a2e8f9204a82592a2efcb6442d70046ad96'
